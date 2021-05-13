d570f97435683fc956ae7d00797bb0a3140e8c87
Jimmy Meriläinen <jimmy@MacBook-Pro-som-tillhor-Jimmy.local>
2021-05-13 15:56:35 +0200

    Version 1.0

---
+++ precache-manifest.4fd6cc11eab496121e45c2766d1ec89a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be97c4ea155e69743e0",
+    "url": "/css/about.0e433876.css"
+  },
+  {
+    "revision": "723d3a6230548f0c5068",
+    "url": "/css/app.db1db10e.css"
+  },
+  {
+    "revision": "68b7200479350bb5fe91",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "21bba8fff0d12fb8e755f80d3be60290",
+    "url": "/img/apple-touch-icon.21bba8ff.png"
+  },
+  {
+    "revision": "2f7e0429df7aacafd7004572e6845395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4be97c4ea155e69743e0",
+    "url": "/js/about.30f3c95a.js"
+  },
+  {
+    "revision": "723d3a6230548f0c5068",
+    "url": "/js/app.e0cb5242.js"
+  },
+  {
+    "revision": "68b7200479350bb5fe91",
+    "url": "/js/chunk-vendors.ed611c2d.js"
+  },
+  {
+    "revision": "d3de35b82ef60093e46e24855b1e7e34",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
